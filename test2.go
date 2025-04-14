e9b8444bbcff53b3c969db900172db1f3425c0f4
kanaji <gellunika6478@icloud.com>
2025-04-14 14:12:35 +0900

    elseは前のifと同じ行じゃないと動かないらしい．

--- test2.go
+++ test2.go
@@ -6,4 +6,19 @@ myslice := []int {1,2,3,3}
 fmt.Println(myslice)
 fmt.Println(myslice[0])
 
+
+var a int = 10
+var b int = 20
+
+if a>b {
+
+fmt.Println("a is greater than b")
+}else {
+fmt.Println("a is less than b")
+}
+
+
+a&=5
+fmt.Println(a)
+
 }
